49c6d51a33455f94a4135d76b6ee7e297d1d317c
mehdiznaidi <mmznd6@gmail.com>
2025-11-08 14:59:15 +0100

    Update partie1.s

--- partie1.s
+++ partie1.s
@@ -1,7 +1,7 @@
 # Partie 1 : La pause
-# Programme qui affiche les entiers de 1 à 10
+# Programme qui affiche les entiers de 1 Ã  10
 # avec une pause de 500ms entre chaque affichage
-# Durée totale : 5 secondes
+# DurÃ©e totale : 5 secondes
 
 .data
     newline: .string "\n"
@@ -10,36 +10,35 @@
 .globl main
 
 main:
-    # Initialiser le compteur à 1
-    li t0, 1               # t0 = compteur (commence à 1)
-    li t1, 11              # t1 = limite (s'arrête à 10)
-
+    # Initialiser le compteur Ã  1
+    li t0, 1               # ont met t0 = 1 pour commencer la boucle 
+    li t1, 11              # t1 va etre notre arret de boucle 
 boucle:
-    # Vérifier si on a atteint 11 (donc fini avec 10)
-    bge t0, t1, fin        # Si t0 >= 11, terminer
+    # VÃ©rifier si on a atteint 11 , car la focntion s arrete a 10
+    bge t0, t1, fin        # Si t0 >= 11, on saute vers fin
     
     # Afficher la valeur de t0
-    mv a0, t0              # Copier t0 dans a0 pour l'affichage
-    li a7, 1               # Syscall 1 : print_int
+    mv a0, t0              # on copie ici t0 dans a0 pour l'affichage
+    li a7, 1               # 1 en a7 = print_int
     ecall
     
-    # Afficher un retour à la ligne
+    # Afficher un retour Ã  la ligne
     la a0, newline
-    li a7, 4               # Syscall 4 : print_string
+    li a7, 4               # 4 = print_string
     ecall
     
-    # Pause de 500 millisecondes
-    li a0, 500             # Charger 500 dans a0
-    li a7, 32              # Syscall 32 : sleep (en millisecondes)
+    # Pause de 500 ms
+    li a0, 500             #  On met 500  dans a0 pour la pause 
+    li a7, 32              # 32 = sleep (en millisecondes)
     ecall
     
-    # Incrémenter le compteur
+    # IncrÃ©menter le compteur
     addi t0, t0, 1         # t0 = t0 + 1
     
-    # Retour au début de la boucle
+    # Retour au dÃ©but de la boucle
     j boucle
 
 fin:
     # Terminer le programme
-    li a7, 10              # Syscall 10 : exit
+    li a7, 10              # 10 = exit
     ecall
